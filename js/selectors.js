e5bc25344f6e78246a29e9a2ea1332e5384b62d4
Jatupon Jantri <jameball007@gmail.com>
2018-02-21 08:22:56 +0000

    automatically backed up by learn

--- js/selectors.js
+++ js/selectors.js
@@ -1,2 +1,20 @@
 // declare your functions here...
 
+function paragraphSelector() {
+  var selectorP = $('p')
+  return selectorP
+}
+
+function lastImageSelector() {
+  var lastImage = $('img:last')
+  return lastImage
+}
+
+function ninjaBabySelector() {
+  var id 
+}
+
+function divSelector() {
+  var classPics = $('.pics')
+  return classPics
+}
